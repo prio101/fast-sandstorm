7bef99cddb9724b94bc5cc5c577836fe4b96f766
prio101 <miprio101@gmail.com>
2021-02-26 11:51:58 +0600

    Added the migration and the gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "problems#index"
-  
-  resources :users, only: [:show, :edit, :update]
+
+  resources :users, except: [ :destroy, :index, :new, :create ]
   resources :problems
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_043020) do
+ActiveRecord::Schema.define(version: 2021_02_26_054919) do
 
   create_table "problems", force: :cascade do |t|
     t.string "name"
@@ -23,11 +23,17 @@ ActiveRecord::Schema.define(version: 2021_02_25_043020) do
 
   create_table "users", force: :cascade do |t|
     t.string "full_name"
-    t.string "email"
     t.string "github"
     t.string "twitter"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
