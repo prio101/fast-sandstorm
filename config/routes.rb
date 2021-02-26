fea2fd02289ae2ee1397e152c9f6e93f5ddf1008
prio101 <miprio101@gmail.com>
2021-02-26 11:42:15 +0600

    added the only required methods users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "problems#index"
-  resources :users
+  
+  resources :users, only: [:show, :edit, :update]
   resources :problems
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
