1d97a0097ee1cff27e7abc9cb36c9dbe065d8994
prio101 <miprio101@gmail.com>
2021-03-17 21:33:29 +0600

    Added the status badge color helper

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -1,2 +1,14 @@
 module ProblemsHelper
+  def status_color(status)
+    case status
+    when "completed"
+      "green"
+    when "ongoing"
+      "blue"
+    when "postponed"
+      "yellow"
+    else
+      "gray"
+    end
+  end
 end
